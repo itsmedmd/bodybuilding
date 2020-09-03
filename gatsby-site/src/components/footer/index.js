1b8a3c45f1bd1901322293826f31c6bc14f81a52
ElqBell <silenc8@gmail.com>
2020-09-03 20:36:30 +0300

    added videos

--- gatsby-site/src/components/footer/index.js
+++ gatsby-site/src/components/footer/index.js
@@ -5,7 +5,7 @@ export default function Footer() {
       <footer className="animate-on-scroll" >
           <h1>
               'Zyzz is not a person. Zyzz is merely a personification of a way of life.'
-              <span>-Aziz</span>
+              <span>-Aziz Sergeyevich Shavershian</span>
           </h1>
           <picture>
             <source media="(max-width: 1024px)" srcSet="quotes-clear.jpg" />
@@ -14,24 +14,3 @@ export default function Footer() {
       </footer>
   );
 }
-
-/*
-    <div>
-        <p>"Everybody, one day will die, and be forgotten. Act and behave in a way that will make life interesting and fun, fuck a mundane predictable life working monday to friday with something you derive no pleasure from; just living life out till you grow old and wither. Find a passion, form relationships, don't be afraid to get out there and fuck what everyone else thinks, trust me, it's a lot more fun that way."</p>
-        <p>"Because sticking up for yourself makes you look mad. Laughing at their face and saying 'u mad' is a million times more effective."</p>
-        <p>"U mirin'?"</p>
-        <p>"Come at me brah"</p>
-        <p>"My theory is that life is a video game kind of like The Sims, and some person is controlling each and every single one of us, and whoever is controlling me has the top score. U mad?"</p>
-        <p>"Pretend, before each set: someone is holding a gun to your head and you must complete x number of reps or a certain weight to survive. You will watch yourself and push harder than normal."</p>
-        <p>"FUUUAAARRRK!!"</p>
-        <p>"I still train when I'm sick, unless you are running a fever, then don't train. A cold? Runny nose? I still train. If I'm sore, I still train. If I'm tired, I still train."</p>
-        <p>"Too many people go to the gym, do a few bicep curls and tricep pushdowns and make no progress. Use your mind to motivate you to go HARDOUT every workout, that's what will transform your body. When your body tells you to stop, keep going, because it's your mind that will ultimately make you push through it."</p>
-        <p>"I don't workout for chicks, I workout to create an aura. When I walk into a room, introduce myself to someone, go for a job interview, I love looking dominant, in charge, and in control. I love walking past and having people point and talk about me, I love the fact that when I go somewhere with thousands of people, almost all of them will remember who I am when it is over, and I didn't even have to say a word."</p>
-        <p>"I don't stress for exams. I stress more when I have a cheat meal."</p>
-        <p>"My message is to train hard, don't be a hard cunt, enjoy life and don't take yourself too seriously. If there were more people like me, the world would be a happier place. I'd rather do what I do and have fun than sit back hating on someone achieving something."</p>
-        <p>"U mad?"</p>
-        <p>"The main thing you need to do now is stick with it. It’s an ongoing process, a life-style if you will, and it takes time and commitment. If you diet right and train hard, you’re guaranteed results, it’s impossible not to grow."</p>
-        <p>"Eat the right foods and the right amount. Train right - don’t cheat yourself. Supplement where you need to. Push yourself to the limit."</p>
-        <p>"Good luck, and I hope that some day I’ll be coming to you for tips."</p>
-    </div>
-*/
--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -18,17 +18,23 @@ export default function Home() {
           entry.target.classList.add("is-visible");
       });
     };
-    
-    const observer = new IntersectionObserver(callback);
-    
+
     const targets = document.getElementsByClassName("animate-on-scroll");
     const targetsArray = [];
     for(let i = 0; i < targets.length; i++)
       targetsArray.push(targets[i]);
 
-    targetsArray.forEach(function(target) {
-      observer.observe(target);
-    });
+    // IntersectionObserver doesn't exist in IE so try...catch is needed
+    try {
+      const observer = new IntersectionObserver(callback);
+      targetsArray.forEach(function(target) {
+        observer.observe(target);
+      });
+    } catch(error) {
+      targetsArray.forEach(function(target) {
+        target.classList.add("is-visible");
+      });
+    }
   }, []);
 
   return (
@@ -46,12 +52,16 @@ export default function Home() {
           <br/>
           <p>Zyzz naturally had amazing genetics and with years hard work you could see his transformation was complete - he had become a The God of aesthetics. Ater getting recognized on the internet, after some time he was starting to get so popular that he had established a clothing line and even his own protein label, Protein of the Gods.</p>
         </Chapter>
-        <Chapter number="THREE" title="Inspiration" src="brothers.jpg" lazySrc="brothers-lowq.jpg" alt="Zyzz and his brother Chestbrah smiling" >
+        <Chapter number="THREE" title="Genesis" src="brothers.jpg" lazySrc="brothers-lowq.jpg" alt="Zyzz and his brother Chestbrah smiling" >
           <p>But what started this bodybuilding journey? It was none other than Zyzz's bodybuilder brother known as Chestbrah. Zyzz began training in his local gym where he learned many things unknown to him about nutrition and training, applying it to his pursuit of becoming The God of aesthetics.</p>
           <br/>
           <p>His favourite professional bodybuilders were Arnold Schwarzenegger and Frank Zane. In an interview with bodybuilding website, Simplyshredded.com, Shavershian recalled that he originally wanted to become a bodybuilder to "impress girls."</p>
           <br/>
           <p>Almost four years into training, he stated that: "I can safely say that my motivation to train goes far beyond that of merely impressing people, it is derived from the feeling of having set goals and achieving them and outdoing myself in the gym. I absolutely love it, the feeling of pushing out that last rep, and getting skin tearing pumps is something I don't see myself without."</p>
+          <br/>
+          <div className="iframe-container">
+            <iframe src="https://www.youtube.com/embed/6jg-PaQfyac"></iframe>
+          </div>
         </Chapter>
         <Chapter number="FOUR" title="The Legacy" src="bible.jpg" lazySrc="bible-lowq.jpg" alt="Zyzz's bodybuilding bible cover" >
           <p>Zyzz's influence in bodybuilding and aesthetics communities has been so great that many people have been inspired by him to train, not give a fuck about what other people think and live life to the fullest.</p>
@@ -59,6 +69,10 @@ export default function Home() {
           <p>There are countless videos that people watch every single day of Zyzz doing just that. Watching those videos and seeing The God of aesthetics going around burning with confidence and not giving a fuck is a sight to behold.</p>
           <br/>
           <p>Although there are many videos of Zyzz from others' perspectives, there is one great bodybuilding resource that is written by Zyzz himself - Zyzz's Bodybuilding Bible. In this book Zyzz shares all his knowledge of bodybuilding that a bodybuilder should know - from diet, training and supplements to advanced training techniques and bodybuilder lifestyle.</p>
+          <br/>
+          <div className="iframe-container">
+            <iframe src="https://www.youtube.com/embed/AdBoybKnzZw"></iframe>
+          </div>
         </Chapter>
         <Chapter number="FIVE" title="Conclusion" src="gravestone.jpg" lazySrc="gravestone-lowq.jpg" alt="Chestbrah next to Zyzz's gravestone" >
           <p>“The flame that burns Twice as bright burns half as long.” ― Lao Tzu, Te Tao Ching</p>
